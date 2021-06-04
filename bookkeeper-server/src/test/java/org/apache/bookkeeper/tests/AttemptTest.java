065eae79b08776b69a09ed6fbe306e38762e41ad
Michele <michelesalvv@gmail.com>
2021-06-04 18:20:18 +0200

    test

---
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/tests/AttemptTest.java
@@ -0,0 +1,15 @@
+package org.apache.bookkeeper.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AttemptTest {
+
+	@Test
+	public void test() {
+		boolean result = true;
+		assertTrue(result);
+	}
+
+}
