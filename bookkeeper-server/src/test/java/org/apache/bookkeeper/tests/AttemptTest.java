9fe19e857a07dbe897c6e0dd00dda07bce7636c8
Michele <michelesalvv@gmail.com>
2021-06-05 12:32:59 +0200

    modules deactivated

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/tests/AttemptTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/tests/AttemptTest.java
@@ -1,15 +1,27 @@
 package org.apache.bookkeeper.tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import java.io.IOException;
+
+import org.apache.bookkeeper.bookie.EntryKey;
+import org.apache.bookkeeper.bookie.SortedLedgerStorage;
 import org.junit.Test;
 
 public class AttemptTest {
 
-	@Test
-	public void test() {
-		boolean result = true;
-		assertTrue(result);
-	}
-
+	 @Test
+	    public void dummyTest() throws IOException {
+	        SortedLedgerStorage srl = new SortedLedgerStorage();
+	        System.out.println(srl);
+	        Long id = 4000L;
+//	        ByteBuf ledger = LedgerDescriptor.createLedgerFenceEntry(id);
+	        EntryKey entry = new EntryKey();
+	        System.out.println(entry);
+	        
+	        int parameter = 5;
+	        assertEquals(5, parameter);
+	    }
 }
+
+
